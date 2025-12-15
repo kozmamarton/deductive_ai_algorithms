65ae09ded48467e636ebad9924422c29a2a1eaf3
Kozma Márton <kozmamarton76@gmail.com>
2025-12-15 18:33:25 +0100

    Agent races itself. Great for debugging

--- tier_3/start.ps1
+++ tier_3/start.ps1
@@ -6,7 +6,7 @@ param(
 $workdir = "judge/"
 $config_file = "$workdir/sample_config.json"
 $bots_dir = "bot/"
-$replay_arg = ""
+$replay_arg = " "
 
 if ($player_num -le 0) {
     Write-Host "No player number given! Example usage: .\start.ps1 <player_number>"
@@ -16,7 +16,9 @@ if ($player_num -le 0) {
 Write-Host "starting judge with $player_num players..."
 
 if ($replay_flag -eq "--replay") {
-    $replay_arg = "--replay_file ./last_replay"
+    $replay_arg = "--replay_file ./last_replay1"
+}else{
+    $replay_flag = ""
 }
 
 Start-Process -FilePath "python" -ArgumentList "$($workdir)run.py", $config_file, $player_num, $replay_arg -NoNewWindow
@@ -25,7 +27,13 @@ Start-Sleep -Seconds 3
 Write-Host "starting players"
 
 Start-Process -FilePath "python" -ArgumentList "$($bots_dir)client_bridge.py", "bot/winnerBot/bot.py" -NoNewWindow
-#Start-Sleep -Seconds 1
+Start-Sleep -Seconds 1
+Start-Process -FilePath "python" -ArgumentList "$($bots_dir)client_bridge.py", "bot/winnerBot/bot.py" -NoNewWindow
+Start-Sleep -Seconds 1
+Start-Process -FilePath "python" -ArgumentList "$($bots_dir)client_bridge.py", "bot/winnerBot/bot.py" -NoNewWindow
+Start-Sleep -Seconds 1
+Start-Process -FilePath "python" -ArgumentList "$($bots_dir)client_bridge.py", "bot/winnerBot/bot.py" -NoNewWindow
+Start-Sleep -Seconds 1
 #Start-Process -FilePath "python" -ArgumentList "$($bots_dir)client_bridge.py", "bot/lieutenant_crown_him_with_many_crowns_thy_full_gallant_legions_he_found_it_in_him_to_forgive.py" -NoNewWindow
 
 
@@ -37,7 +45,7 @@ if (-not $replay_arg) {
 }
 
 Write-Host "Race complete!"
-$last_log = "last_replay"
+$last_log = "last_replay1"
 Write-Host $last_log
 python visualisation.py $last_log --visibility_radius 8
 
